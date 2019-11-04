e8c9719ca5d1541cd72be581ef62bc5cb8d1e7f9
Maba Jarbou <Jarbou@Air-van-Maba.lan>
2019-11-04 12:03:30 +0100

    ga

--- index.php
+++ index.php
@@ -216,5 +216,14 @@
         duration: 1200
       });
   	</script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LF644BMWJN"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-LF644BMWJN');
+    </script>
   </body>
 </html>
